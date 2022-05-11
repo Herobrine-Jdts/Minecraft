eead295c86672e03e45e41b3d27c4bdd47adb018
Herobrine-Jdts <60424367+Herobrine-Jdts@users.noreply.github.com>
2022-05-11 16:53:53 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,17 +1,35 @@
 @echo off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+set md5_road="D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\Start\md5.md5"
+
 rem crack里的文件替换到游戏文件夹
-copy /y "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\Crack\AdvGuide\ELDEN RING Adventure Guide_Data\Plugins\x86_64\steam_api64.dll" "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\AdvGuide\ELDEN RING Adventure Guide_Data\Plugins\x86_64\steam_api64.dll"
-copy /y "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\Crack\ArtbookOST\ELDEN RING Digital Artbook & Soundtrack_Data\Plugins\x86_64\steam_api64.dll" "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\ArtbookOST\ELDEN RING Digital Artbook & Soundtrack_Data\Plugins\x86_64\steam_api64.dll"
-copy /y "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\Crack\Game\steam_api64.dll" "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\Game\steam_api64.dll"
+set AdvGuide="D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\AdvGuide\ELDEN RING Adventure Guide_Data\Plugins\x86_64\steam_api64.dll"
+certutil -hashfile %AdvGuide% MD5 | find /v ":" > %md5_road%
+set /p md5=<%md5_road%
+if not "23390ac384575350c1286bb357666b81" == "%md5%" (copy /y "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\Crack\AdvGuide\ELDEN RING Adventure Guide_Data\Plugins\x86_64\steam_api64.dll" %AdvGuide%)
+del /q %md5_road%
+
+set ArtbookOST="D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\ArtbookOST\ELDEN RING Digital Artbook & Soundtrack_Data\Plugins\x86_64\steam_api64.dll"
+certutil -hashfile %ArtbookOST% MD5 | find /v ":" > %md5_road%
+set /p md5=<%md5_road%
+if not "23390ac384575350c1286bb357666b81" == "%md5%" (copy /y "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\Crack\ArtbookOST\ELDEN RING Digital Artbook & Soundtrack_Data\Plugins\x86_64\steam_api64.dll" %ArtbookOST%)
+del /q %md5_road%
+
+set Game="D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\Game\steam_api64.dll"
+certutil -hashfile %Gmae% MD5 | find /v ":" > %md5_road%
+set /p md5=<%md5_road%
+if not "23390ac384575350c1286bb357666b81" == "%md5%" (copy /y "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\Crack\Game\steam_api64.dll" %Game%)
+del /q %md5_road%
+
 rem 判断是否已开启Anti-cheat
 :label
-set md5=6ea87d309cbda60c53948769b8d595c4
-set road="D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\Start\md5.md5"
-certutil -hashfile "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\Game\start_protected_game.exe" MD5 | find /v ":" > %road%
-set /p now_md5=<%road%
-if not "%md5%" == "%now_md5%" (
+set cheat="D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\Game\start_protected_game.exe"
+certutil -hashfile %cheat% MD5 | find /v ":" > %md5_road%
+set /p md5=<%md5_road%
+if not "6ea87d309cbda60c53948769b8d595c4" == "%md5%" (
 	start /w /d "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\Game" "" "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\Game\toggle_anti_cheat.exe"
 	goto label)
+del /q %md5_road%
+
 rem 打开FPS解锁工具
 start "" "D:\Documents\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\Start\EldenRingFPSUnlockAndMore.exe"
